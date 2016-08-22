f242efc5c2fe07c6a5b44c98a8fbf9431199df7d
Alex Bang (MBP) <alexbang@hotmail.com>
2016-08-22 12:23:13 -0400

    Finish writing code

--- exercise.js
+++ exercise.js
@@ -1,10 +1,19 @@
 class HashMap {
-  constructor () {
+  constructor (initialCapacity) {
     this.length = 0
     this._slots = []
     this._capacity = initialCapacity || 8
     this.MAX_LOAD_RATIO = 0.9
     this.SIZE_RATIO = 3
+    this._deleted = 0
+  }
+
+  get (key) {
+    let index = this._findSlot(key)
+    if (this._slots[index] === undefined) {
+      throw new Error('key error')
+    }
+    return this._slots[index].value
   }
 
   _hashString (string) {
@@ -17,7 +26,7 @@ class HashMap {
   }
 
   set (key, value) {
-    let loadRatio = (this.length + 1) / this.capacity
+    let loadRatio = (this.length + this._deleted + 1) / this.capacity
     if (loadRatio > this.MAX_LOAD_RATIO) {
       this._resize(this._capacity * this.SIZE_RATIO)
     }
@@ -25,19 +34,31 @@ class HashMap {
     let index = this._findSlot(key)
     this._slots[index] = {
       key,
-      value
+      value,
+      deleted: false
     }
     this.length++
   }
 
+  remove (key) {
+    let index = this._findSlot(key)
+    let slot = this.slots[index]
+    if (slot === undefined) {
+      throw new Error('key error')
+    }
+    slot.deleted = true
+    this.length--
+    this._deleted++
+  }
+
   _findSlot (key) {
-    let hash = this._hashString(key)
+    let hash = HashMap._hashString(key)
     let start = hash % this._capacity
 
     for (let i = start; i < start + this._capacity; i++) {
       let index = i % this._capacity
       let slot = this._slots[index]
-      if (slot === undefined || slot.key == key) {
+      if (slot === undefined || (slot.key == key && !slot.deleted)) {
         return index
       }
     }
@@ -51,7 +72,7 @@ class HashMap {
     this._slots = []
     for (let i = 0; i < oldSlots.length; i++) {
       let slot = oldSlots[i]
-      if (slot !== undefined) {
+      if (slot !== undefined && !slot.deleted) {
         this.set(slot.key, slot.value)
       }
     }
